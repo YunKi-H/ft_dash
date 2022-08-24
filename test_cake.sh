b93e18785e3a81f81e286878ca4b5e54f60312fb
niamu01 <niamu01@naver.com>
2022-08-24 12:59:55 +0900

    add: test case

--- test_cake.sh
+++ test_cake.sh
@@ -24,6 +24,9 @@ make re
 #OK
 ./vpdlwldkfrhflwma 1 2 3 4 5 6 7 1 2 3 4 5 6 7  | ./corrector 1 2 3 4 5 6 7 1 2 3 4 5 6 7
 
+#OK
+ARG=`jot -r 500 1 200 | tr '\n' ' '` ; ./vpdlwldkfrhflwma $ARG | ./corrector $ARG
+
 #OK (ctrl-d)
 ./vpdlwldkfrhflwma 
 ./corrector 
